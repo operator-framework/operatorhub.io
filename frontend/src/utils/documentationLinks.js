06a905c43160a36a791e347bb6f08fa5b84a330f
Eric Stroczynski <ericstroczynski@gmail.com>
2019-04-30 13:30:08 -0700

    frontend/src: fix Contribute page text and examples

--- frontend/src/utils/documentationLinks.js
+++ frontend/src/utils/documentationLinks.js
@@ -1,6 +1,8 @@
 const operatorsFramework = 'https://github.com/operator-framework';
 const operatorsRepo = `${operatorsFramework}/community-operators`;
 const contributions = `${operatorsRepo}/tree/master/upstream-community-operators`;
+const operatorsRepoBeforePR = `${operatorsRepo}#before-submitting-a-pr`;
+const operatorsRepoRequirements = `${operatorsRepo}#know-what-to-contribute`;
 const operatorSdk = `${operatorsFramework}/operator-sdk`;
 const operatorCourier = `${operatorsFramework}/operator-courier`;
 const operatorScorecard = `${operatorSdk}/blob/master/doc/test-framework/scorecard.md`;
@@ -9,6 +11,7 @@ const olm = `${operatorsFramework}/operator-lifecycle-manager`;
 const olmGettingStarted = `${olm}#getting-started`;
 const gettingStarted = `${operatorsFramework}/getting-started`;
 const operatorRegistry = `${operatorsFramework}/operator-registry`;
+const operatorRegistrySteps = `${operatorRegistry}#using-the-catalog-with-operator-lifecycle-manager`;
 const marketplaceRef = `${operatorsFramework}/operator-marketplace`;
 const prometheusOperator = `${contributions}/prometheus`;
 const olmArchitecture = `${olm}/blob/master/Documentation/design/architecture.md`;
@@ -20,7 +23,6 @@ const sampleCode = `${operatorsFramework}/operator-sdk-samples/blob/master/memca
 const capabilityLevelModelDiagram = `${operatorSdk}/blob/master/doc/images/operator-maturity-model.png`;
 const manageOperatorWithOlm = `${operatorsFramework}/getting-started#manage-the-operator-using-the-operator-lifecycle-manager`;
 const operatorGroupDesign = `${olm}/blob/master/Documentation/design/architecture.md#operator-group-design`;
-const addingYourOperator = `${operatorsRepo}#adding-your-operator`;
 const privacyPolicy = `https://www.redhat.com/en/about/privacy-policy`;
 const kubernetesSlack = `https://kubernetes.slack.com/messages/kubernetes-operators`;
 const hubFacebook = ``;
@@ -32,6 +34,8 @@ const DocmentationLinks = {
   operatorsFramework,
   operatorsRepo,
   contributions,
+  operatorsRepoBeforePR,
+  operatorsRepoRequirements,
   operatorSdk,
   operatorCourier,
   operatorScorecard,
@@ -40,6 +44,7 @@ const DocmentationLinks = {
   olmGettingStarted,
   gettingStarted,
   operatorRegistry,
+  operatorRegistrySteps,
   marketplaceRef,
   prometheusOperator,
   olmArchitecture,
@@ -48,7 +53,6 @@ const DocmentationLinks = {
   discoveryCatalogs,
   introBlog,
   sampleCode,
-  addingYourOperator,
   capabilityLevelModelDiagram,
   manageOperatorWithOlm,
   operatorGroupDesign,
@@ -65,6 +69,8 @@ export {
   operatorsFramework,
   operatorsRepo,
   contributions,
+  operatorsRepoBeforePR,
+  operatorsRepoRequirements,
   operatorSdk,
   operatorCourier,
   operatorScorecard,
@@ -73,6 +79,7 @@ export {
   olmGettingStarted,
   gettingStarted,
   operatorRegistry,
+  operatorRegistrySteps,
   marketplaceRef,
   prometheusOperator,
   olmArchitecture,
@@ -81,7 +88,6 @@ export {
   introBlog,
   discoveryCatalogs,
   sampleCode,
-  addingYourOperator,
   capabilityLevelModelDiagram,
   manageOperatorWithOlm,
   operatorGroupDesign,
