8f1417416fe16d55d084d94efc9bab3c974b0dcc
Jozef Radonak <jozef.radonak@hotovo.org>
2019-07-12 13:05:05 +0200

    rho-144 reworked static files loading so they do not need custom express middleware which required manual configuration for every path structure fixed build command to directly call webpack as it was failing in Jenkins

--- server/routes/uiRoutes.js
+++ server/routes/uiRoutes.js
@@ -1,60 +1,13 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 
 const testRouteFile = path.resolve(__dirname, '../../test-route/akamai-sureroute-test-object.html');
-
-const getDistFilePath = fileName => {
-  const distDir = path.resolve(__dirname, '../../frontend/dist');
-  if (!fileName) {
-    fileName = 'index.html';
-  }
-  const filePath = path.join(distDir, fileName);
-  if (fs.existsSync(filePath)) {
-    return filePath;
-  }
-
-  let subFileName = fileName;
-  while (subFileName.indexOf('/') !== -1) {
-    subFileName = subFileName.slice(fileName.indexOf('/') + 1);
-    const subfilePath = path.join(distDir, subFileName);
-    if (fs.existsSync(subfilePath)) {
-      return subfilePath;
-    }
-  }
-
-  return path.join(distDir, 'index.html');
-};
-
-const addRootRedirect = (app, pathName) => {
-  app.get(`*/${pathName}`, (request, response) => {
-    const distFilePath = getDistFilePath(request.url.slice(pathName.length + 2));
-    response.sendFile(distFilePath);
-  });
-  app.get(`*/${pathName}/*`, (request, response) => {
-    const distFilePath = getDistFilePath(request.url.slice(pathName.length + 2));
-    response.sendFile(distFilePath);
-  });
-};
+const indexFile = path.resolve(__dirname, '../../frontend/dist/index.html');
+const distFolder = path.resolve(__dirname, '../../frontend/dist/');
 
 module.exports = app => {
-  // Base Public Routes
-  const uiRoutes = [
-    'operator',
-    'preview',
-    'bundle',
-    'about',
-    'contribute',
-    'getting-started',
-    'what-is-an-operator',
-    'how-to-install-an-operator'
-  ];
-
-  app.use(express.static('../frontend/dist'));
-
-  // Page Routes
-  uiRoutes.forEach(uiRoute => {
-    addRootRedirect(app, uiRoute);
+  app.get('/', (request, response) => {
+    response.sendFile(indexFile);
   });
 
   app.use('/static', express.static(distFolder));
