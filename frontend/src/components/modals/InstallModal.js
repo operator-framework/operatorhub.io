17da603fba327dea73c0616f5c476a8f9e2c44ab
Jozef Radonak <jozef.radonak@hotovo.org>
2019-07-29 15:22:19 +0200

    Updated install modal to always show prerequisities

--- frontend/src/components/modals/InstallModal.js
+++ frontend/src/components/modals/InstallModal.js
@@ -6,7 +6,6 @@ import copy from 'copy-to-clipboard';
 import { Icon, Modal } from 'patternfly-react';
 import { CatalogItemHeader } from 'patternfly-react-extensions';
 
-import { helpers } from '../../common/helpers';
 import * as operatorImg from '../../imgs/operator.svg';
 import { InternalLink } from '../InternalLink';
 
