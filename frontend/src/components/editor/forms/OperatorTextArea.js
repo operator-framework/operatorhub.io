5e1ebdead9e910e067f93dabbe0a73c8ce6797a6
Jozef Radonak <jozef.radonak@hotovo.org>
2019-08-06 11:33:59 +0200

    rho-184 refactored common inputs and text area usage to remove code duplication and use shared component instead of similar implementations
    removed bug which rendered input instead of text area

--- frontend/src/components/editor/forms/OperatorTextArea.js
+++ frontend/src/components/editor/forms/OperatorTextArea.js
@@ -11,23 +11,22 @@ import { helpers } from '../../../common/helpers';
  * @param {object} param0
  * @param {string} param0.title
  * @param {string} param0.field
- * @param {string|string[]|null} param0.value
- * @param {string} param0.inputType
+ * @param {string|string[]|null} [param0.value]
  * @param {*} param0.formErrors
  * @param {import('./OperatorInputWrapper').UpdaterOperatorFromInputCallback} param0.updateOperator
  * @param {import('./OperatorInputWrapper').CommitOperatorFieldFromInputCallback} param0.commitField
+ * @param {string|null} [param0.defaultValue]
  * @param {Function=} [param0.refCallback]
  */
-const OperatorInput = ({ title, field, inputType, formErrors, value, updateOperator, commitField, refCallback }) => (
+const OperatorTextArea = ({ title, field, formErrors, value, updateOperator, commitField, refCallback }) => (
   <OperatorInputWrapper title={title} field={field} formErrors={formErrors}>
-    <input
+    <textarea
       id={field}
       className="form-control"
-      type={inputType}
       rows={3}
       {..._.get(_.get(operatorFieldValidators, field), 'props')}
-      onBlur={e => commitField(field, e.target.value)}
       onChange={e => updateOperator(e.target.value, field)}
+      onBlur={e => commitField(field, e.target.value)}
       placeholder={_.get(operatorFieldPlaceholders, field)}
       value={value}
       ref={refCallback}
@@ -35,10 +34,9 @@ const OperatorInput = ({ title, field, inputType, formErrors, value, updateOpera
   </OperatorInputWrapper>
 );
 
-OperatorInput.propTypes = {
+OperatorTextArea.propTypes = {
   title: PropTypes.string.isRequired,
   field: PropTypes.string.isRequired,
-  inputType: PropTypes.string.isRequired,
   formErrors: PropTypes.any.isRequired,
   commitField: PropTypes.func.isRequired,
   updateOperator: PropTypes.func.isRequired,
@@ -46,8 +44,8 @@ OperatorInput.propTypes = {
   refCallback: PropTypes.func
 };
 
-OperatorInput.defaultProps = {
+OperatorTextArea.defaultProps = {
   refCallback: helpers.noop
 };
 
-export default OperatorInput;
+export default OperatorTextArea;
