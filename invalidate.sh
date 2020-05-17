53cc930c25b83a0c1ecd8bf193f525ad314a8638
Eduard Kracmar <ekracmar@redhat.com>
2020-05-17 12:43:16 +0200

    RHO-80: typo

--- invalidate.sh
+++ invalidate.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p bin
 if ! ./bin/akamai-purge >/dev/null 2>&1; then
     echo Installing akamai-purge
-    wget -Lo bin/akamai-purge https://github.com/akamai/cli-purge/releases/download/1.0.1/akamai-purge-1.0.1-linuxamd64
+    curl -Lo bin/akamai-purge https://github.com/akamai/cli-purge/releases/download/1.0.1/akamai-purge-1.0.1-linuxamd64
     chmod +x bin/akamai-purge
 fi
 
