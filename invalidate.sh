db489983c31112c193cd39b97bd3a1d2bac70c1d
Eduard Kracmar <ekracmar@redhat.com>
2020-05-17 11:28:21 +0200

    RHO-80: Switched to akamai-purge

--- invalidate.sh
+++ invalidate.sh
@@ -3,22 +3,17 @@
 set -e
 
 mkdir -p bin
-if ! ./bin/egcurl >/dev/null 2>&1; then
-    echo Installing egcurl
-    curl -o bin/egcurl https://raw.githubusercontent.com/akamai/edgegrid-curl/a69a71a133a539df3195f82a56664b21bc9beb0b/egcurl
-    pip2 install --user edgegrid-python
+if ! ./bin/akamai-purge >/dev/null 2>&1; then
+    echo Installing akamai-purge
+    wget -Lo bin/akamai-purge https://github.com/akamai/cli-purge/releases/download/1.0.1/akamai-purge-1.0.1-linuxamd64
+    chmod +x bin/akamai-purge
 fi
 
-if ! cat ~/.edgerc >/dev/null ; then
+if [ ! -f ~/.edgerc ]; then
     echo Setting up ~/.edgerc
     echo $AKAMAI_SECRETS | base64 -d > ~/.edgerc
-    cat ~/.edgerc
 fi
 
-python2 bin/egcurl -sSik \
-    --eg-section ccu \
-    -X POST \
-    --data-binary "{\"objects\":[$AKAMAI_APPCODES]}" \
-    -H "Content-type: application/json" \
-    -H "Accept: application/json" \
-    "https://$AKAMAI_HOST/ccu/v3/invalidate/cpcode/production"
+bin/akamai-purge \
+    invalidate \
+    --cpcode $AKAMAI_APPCODES
