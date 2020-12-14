580615113c45e1a8936bb9301d5fe0701ea528dd
Eduard Kracmar <ekracmar@redhat.com>
2020-12-14 08:04:58 +0100

    RHO-857: migration of CICD to GH Actions

--- deploy.sh
+++ deploy.sh
@@ -10,26 +10,35 @@ if [[ ! "$ENVIRO" =~ ^(pica|dev|preprod|prod)$ ]]; then
 fi
 
 mkdir -p bin
-if ! ./bin/oc >/dev/null ; then
-    echo Installing openshift origin client
-    wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-    tar -xvf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+if ! /usr/local/bin/oc > /dev/null 2>&1 ; then
+    echo openshift origin client not found, installing
+    curl -#L -o openshift.tar.gz https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+    tar -xvf openshift.tar.gz --wildcards '*oc'
     mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc bin/
 fi
 
-if ! ./bin/kustomize >/dev/null; then
-    echo Installing kustomize
-    wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.5.4/kustomize_v3.5.4_linux_amd64.tar.gz
-    tar -xvf kustomize_v3.5.4_linux_amd64.tar.gz
+if ! /usr/local/bin/kustomize > /dev/null 2>&1 ; then
+    echo kustomize binary not found, installing
+    curl -L# -o kustomize.tar.gz https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.5.4/kustomize_v3.5.4_linux_amd64.tar.gz
+    tar -xvf kustomize.tar.gz
     mv kustomize bin
+fi
+
+if [[ "$ENVIRO" == "dev" ]]; then
+    OPENSHIFT_TOKEN=$OPENSHIFT_TOKEN_DEV
 fi
 
 if [[ "$ENVIRO" == "preprod" ]]; then
     OPENSHIFT_TOKEN=$OPENSHIFT_TOKEN_PREPROD
 fi
 
-./bin/oc login --token="$OPENSHIFT_TOKEN" --server="$OPENSHIFT_SERVER" > /dev/null
-./bin/kustomize build k8s/$ENVIRO | sed "s/DEPLOYMENT_RUN_ID/$(uuidgen)/" > manifest.yaml
+if [[ "$ENVIRO" == "prod" ]]; then
+    OPENSHIFT_TOKEN=$OPENSHIFT_TOKEN_PROD
+fi
+
+oc login --token="$OPENSHIFT_TOKEN" --server="$OPENSHIFT_SERVER"
+# oc login --token="$OPENSHIFT_TOKEN" --server="$OPENSHIFT_SERVER" > /dev/null
+kustomize build k8s/$ENVIRO | sed "s/DEPLOYMENT_RUN_ID/$(uuidgen)/" > manifest.yaml
 cat manifest.yaml
-cat manifest.yaml | ./bin/oc apply -f -
-./bin/oc rollout status -w deploy/operatorhubio
+cat manifest.yaml | oc apply -f -
+oc rollout status -w deploy/operatorhubio
