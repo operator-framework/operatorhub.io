f3581737d97734abd668ca3b41a48c0f022b9348
Eduard Kracmar <ekracmar@redhat.com>
2020-12-14 08:04:58 +0100

    RHO-857: migration of CICD to GH Actions

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ if [[ ! "$ENVIRO" =~ ^(dev|preprod|prod)$ ]]; then
 fi
 
 mkdir -p bin
-if ! ./bin/oc > /dev/null 2>&1 ; then
+if ! /usr/local/bin/oc > /dev/null 2>&1 ; then
     echo openshift origin client not found, installing
     curl -#L -o openshift.tar.gz https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
     tar -xvf openshift.tar.gz --wildcards '*oc'
@@ -18,19 +18,28 @@ if ! ./bin/oc > /dev/null 2>&1 ; then
     rm -rd openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit*
 fi
 
-if ! ./bin/kustomize > /dev/null 2>&1 ; then
+if ! /usr/local/bin/kustomize > /dev/null 2>&1 ; then
     echo kustomize binary not found, installing
     curl -L# -o kustomize.tar.gz https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.5.4/kustomize_v3.5.4_linux_amd64.tar.gz
     tar -xvf kustomize.tar.gz
     mv kustomize bin
 fi
 
+if [[ "$ENVIRO" == "dev" ]]; then
+    OPENSHIFT_TOKEN=$OPENSHIFT_TOKEN_DEV
+fi
+
 if [[ "$ENVIRO" == "preprod" ]]; then
     OPENSHIFT_TOKEN=$OPENSHIFT_TOKEN_PREPROD
 fi
 
-./bin/oc login --token="$OPENSHIFT_TOKEN" --server="$OPENSHIFT_SERVER" > /dev/null
-./bin/kustomize build k8s/$ENVIRO | sed "s/DEPLOYMENT_RUN_ID/$(uuidgen)/" > manifest.yaml
+if [[ "$ENVIRO" == "prod" ]]; then
+    OPENSHIFT_TOKEN=$OPENSHIFT_TOKEN_PROD
+fi
+
+oc login --token="$OPENSHIFT_TOKEN" --server="$OPENSHIFT_SERVER"
+# oc login --token="$OPENSHIFT_TOKEN" --server="$OPENSHIFT_SERVER" > /dev/null
+kustomize build k8s/$ENVIRO | sed "s/DEPLOYMENT_RUN_ID/$(uuidgen)/" > manifest.yaml
 cat manifest.yaml
-cat manifest.yaml | ./bin/oc apply -f -
-./bin/oc rollout status -w deploy/operatorhubio
+cat manifest.yaml | oc apply -f -
+oc rollout status -w deploy/operatorhubio
