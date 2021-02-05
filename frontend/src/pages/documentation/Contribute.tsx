88ed1ead58f1f6af6d38521a8007e9fcac472b8e
Tony Wu <tlwu2013@gmail.comexport>
2021-02-06 05:14:36 +0800

    Update Contributes page

--- frontend/src/pages/documentation/Contribute.tsx
+++ frontend/src/pages/documentation/Contribute.tsx
@@ -76,8 +76,8 @@ const Contribute: React.FC<ContributePageProps> = ({ history, ...props }) => {
           </p>
           <p>
             If you have an Operator you would like to contribute to OperatorHub.io, feel free to create a pull request
-            against the above directory. Please read the pull request&nbsp;
-            <ExternalLink href={documentationLinks.operatorsRepoBeforePR} text="requirements" />
+            against the above directory. Please read&nbsp;
+            <ExternalLink href={documentationLinks.operatorsRepoBeforePR} text="submitting your operator" />
             &nbsp;before creating one to ensure all required files are present.
           </p>
           <p>
@@ -85,10 +85,8 @@ const Contribute: React.FC<ContributePageProps> = ({ history, ...props }) => {
             <ExternalLink href={documentationLinks.operatorSdk} text="Operator SDK" />, however it is
             not a requirement. To publish your Operator, it must be built as a binary within a container, and that
             container must be hosted on a publicly accessible&nbsp;
-            <ExternalLink href={documentationLinks.operatorRegistry} text="registry" />. It should be
-            accompanied by some&nbsp;
-            <ExternalLink href={documentationLinks.operatorsRepoRequirements} text="metadata" /> that
-            is used for deploying the Operator using the&nbsp;
+            <ExternalLink href={documentationLinks.operatorRegistry} text="registry" />. The Operator should be&nbsp;
+            <ExternalLink href={documentationLinks.operatorsRepoRequirements} text="packaged" /> so it can be deployed using the&nbsp;
             <ExternalLink href={documentationLinks.olm} text="Operator Lifecycle Manager" /> in
             addition to rendering the Operator’s detail page on OperatorHub.io.
           </p>
@@ -105,78 +103,83 @@ const Contribute: React.FC<ContributePageProps> = ({ history, ...props }) => {
             This component of the&nbsp;
             <ExternalLink href={documentationLinks.gettingStarted} text="Operator Framework" /> is
             deployed on your Kubernetes cluster and will be able to install the Operator via CLI or through a GUI
-            component like embedded OperatorHub in OpenShift. Either way, this requires some catalog data to be created
-            in the form of YAML manifests that follow a specific directory structure.
+            component like embedded OperatorHub in OpenShift.
           </p>
           <p>
-            Let’s take a look at an example from the&nbsp;
-            <ExternalLink href={documentationLinks.contributions} text="community repository" />:
+            Your Operator submission can be formatted either following the <b><code>packagemanifest</code></b> or the
+            newer <b><code>bundle</code></b> format. The former allows to ship your entire Operator with all its versions in
+            one single directory. The latter allows shipping individual releases in container images. Both are supported
+            but mixing of formats within a single Operator is not allowed. You need to decide for one or the other
+            for your entire Operator listing. If you are new to this or you don't have this format yet, refer to our&nbsp;
+            <ExternalLink href={documentationLinks.operatorsRepoRequirements} text="contribution documentation" />.
+          </p>
+          <h3>ClusterServiceVersion</h3>
+          <p>
+            The bulk of your Operator's catalog metadata will reside in a ClusterServiceVersion (CSV) file, of which there
+            is one per version of your Operator. This file contains the specifications of how to deploy your Operator,
+            and information on which CRDs it owns and those it depends on from other Operators. In addition this is
+            where you put a description, logo, version, maturity level, authoring info, links etc. for your Operator.
+            This information will be used to render the detail page on OperatorHub.io.
           </p>
           <p>
-            Your catalog data should live in a directory named after your Operator, e.g. the following files exist
-            for the&nbsp;
-            <ExternalLink href={documentationLinks.prometheusOperator} text="Prometheus Operator" />
-            &nbsp;in a directory called <code>prometheus</code>. Within the directory, we refer to a subdirectory that
-            consists of files with one ClusterServiceVersion (CSV) as a &quot;bundle&quot; (an Operator Version).
-            The subdirectory&apos;s folder name is the same as the semantic version of the Operator., e.g.&nbsp;
-            <ExternalLink href={documentationLinks.prometheusOperatorVersion} text="0.22.2" />,
-            which is typically consist of files with one CSV and the CRDs that defines the owned APIs of the CSV,
-            though additional objects may be included.
+            {`
+            Checkout more details in how to
+            `}
+            <ExternalLink href={documentationLinks.buildYourCSV} text="create a ClusterServiceVersion" />.
           </p>
-          {renderTable()}
           <p>
-            This catalog data will enable OLM to serve, install, and update your Operator in a predictable way instead
-            of requiring cluster maintainers to manually deploy (e.g. <code>kubectl create -f ...</code>) required
-            manifests that contain CRDs, RBAC rules, Service Accounts, Deployments etc. If you want to learn more about
-            how OLM does this, read about it&nbsp;
-            <ExternalLink href={documentationLinks.olmArchitecture} text="here" />.
           </p>
-          <h3>Custom Resource Definitions</h3>
+          <h3>CustomResourceDefinitions</h3>
           <p>
             These should already exist if your Operator watches CRDs. Place each CRD that your Operator owns in it’s own
             YAML file. CRDs that your Operator does not own but watches are handled by OLMs dependency management.
           </p>
-          <h3>Cluster Service Version</h3>
+          <h3>PackageManifest Format</h3>
           <p>
-            {`
-            The bulk of your Operator's catalog metadata will reside in a CSV file, of which there is one per version of
-            your Operator. This file contains the specifications of how to deploy your Operator, and information on
-            which CRDs it owns and those it depends on from other Operators. In addition this is where you put a
-            description, logo, version, maturity level, authoring info, links etc. for your Operator. This information
-            will be used to render the detail page on OperatorHub.io. Follow
-            `}
-            <ExternalLink href={documentationLinks.buildYourCSV} text="these instructions" /> to
-            create this file.
-          </p>
-          <p>
-            If you add CSV files for newer versions of your Operator, don’t forget to use the <code>spec.replaces</code>
-            &nbsp;property to point to the previous version.
-          </p>
-          <h3>Package Manifest</h3>
-          <p>
-            {`
-            The package manifest is a simple list of channels that point to a particular CSV name. This allows OLM to
+            The <b><code>packagemanifest</code></b> is a simple list of channels that point to a particular CSV name. This allows OLM to
             serve the Operator from multiple channels, each named to convey that CSV versions' stability and update
             frequency, e.g. stable and alpha. Your package manifest must have at least one channel. For reference, use
-            one of the examples in the
-            `}
+            one of the examples in the&nbsp;
             <ExternalLink href={documentationLinks.contributions} text="community repository" />.
           </p>
-          <p>Feel free to use existing Community Operators catalog data as a template.</p>
-          <h3>Operator Bundle Editor</h3>
-          <p>
-            You can now create your Operator bundle from Operatorhub.io using the&nbsp;
-            <InternalLink route="/packages" history={history}>
-              bundle editor
-            </InternalLink>
-            . Starting by uploading your Kubernetes YAML manifests, the forms on the page will be populated with all
-            valid information and used to create the new Operator bundle. You can modify or add properties through these
-            forms as well.
+          <p>Checkout more details in how to&nbsp;
+            <ExternalLink href={documentationLinks.createPackageManifest} text="create a release using the packagemanifest format" />.
           </p>
           <blockquote>
-            The Operator bundle editor is now available in beta.&nbsp;
+          <h4>Operator Package Editor</h4>
+          <p>
+            You can now create your Operator packagemanifest from Operatorhub.io using the&nbsp;
+            <InternalLink route="/packages" history={history}>
+              package editor
+            </InternalLink>
+            . Either starting from scratch by uploading your Kubernetes YAML manifests, the editor will populate
+            form fields with all valid information to create the new Operator packagemanifest, or feel free to create
+             from an existing Community Operator Package as a template and modify or add properties through these forms.
+          </p>
+          <p>
+            The Operator package editor is now available in beta.&nbsp;
             <ExternalLink href={documentationLinks.fileAnIssue} indicator>Feedback and questions</ExternalLink> are encouraged.
+          </p>
           </blockquote>
+          <h3>Bundle Format</h3>
+          <p>
+            Alternatively you can use the <b><code>bundle</code></b> format, which is nowadays also the default of the Operator-SDK.
+            The <b><code>bundle</code></b> format has a top-level directory named after your Operator name in the CSV.
+            Inside are sub-directories for the individual bundle, named after the&nbsp;
+            <ExternalLink href={documentationLinks.semanticVersioning} text="semantic versioning" />&nbsp;release of your Operator.
+          </p>
+          <p>Checkout more details in how to&nbsp;
+            <ExternalLink href={documentationLinks.createBundle} text="create a release using the bundle format" />.
+          </p>
+          <h3>Moving from PackageManifest to Bundle Format</h3>
+          <p>
+            Eventually this repository will only accept <b><code>bundle</code></b> format at some point in the future.
+          Also the <b><code>bundle</code></b> format has more features like <b><code>semver</code></b> mode or, in the future,
+          installing bundles directly outside of a catalog.
+          </p>
+          <p>Migration of existing content, irregardless of whether the Operator was created with the SDK, can be achieved
+            with the <b><code>opm</code></b> tool on per Operator version basis. Checkout more details in how to&nbsp;
+            <ExternalLink href={documentationLinks.packagemanifestToBundle} text="move from packagemanifest to bundle format" />.</p>
         </React.Fragment>
       )
     },
@@ -190,10 +193,20 @@ const Contribute: React.FC<ContributePageProps> = ({ history, ...props }) => {
             <InternalLink route="/preview" history={history} text="preview" /> page.
           </p>
           <p>
-            <b>Important:</b> This preview only checks the syntax of your CSV. Please use the&nbsp;
-            <ExternalLink href={documentationLinks.operatorCourier} text="operator-courier" /> utility
-            to validate your Operator&nbsp;
-            <ExternalLink href={documentationLinks.operatorBundle} text="bundle" />.
+            A large part of the information gathered in the CSV is used for user-friendly visualization on OperatorHub.io
+            or components like the embedded OperatorHub in OpenShift. Your work is on display, so please ensure to provide
+            relevant information in your Operator's description, specifically covering:</p>
+          <p>
+            <ul>
+              <li>What the managed application is about and where to find more information</li>
+              <li>The features your Operator and how to use it</li>
+              <li>Any manual steps required to fulfill pre-requisites for running / installing your Operator</li>
+            </ul>
+          </p>
+          <p>
+            <b>Important:</b> This preview only checks the syntax of your CSV. Please check out&nbsp;
+            <ExternalLink href={documentationLinks.operatorMetadataValidation} text="Operator Metadata Validation" />
+            &nbsp;to validate your Operator.
           </p>
         </React.Fragment>
       )
@@ -203,22 +216,28 @@ const Contribute: React.FC<ContributePageProps> = ({ history, ...props }) => {
       content: (
         <React.Fragment>
           <p>
-            If you are developing your Operator with the Operator SDK you can simply create most of this catalog data by
-            running the following command:
+            If you are developing your Operator with the Operator SDK you can leverage its packaging tooling to create
+            a <b><code>bundle</code></b> by running the following command:
           </p>
           <p>
-            <code>$ operator-sdk olm-catalog gen-csv --csv-version xx.yy.zz</code>
+            <code>$ make bundle</code>
           </p>
           <p>
-            {`
-            This will scaffold a CSV within the <code>xx.yy.zz</code> subdirectory of your Operator's$&nbsp;
-            `}
-            <code>deploy</code> directory.
+            The command will create the following bundle artifacts:</p>
+            <p>
+            <ul>
+              <li>a manifests directory (<b><code>bundle/manifests</code></b>) containing a CSV and all CRDs from config/crds</li>
+              <li>a&nbsp;
+              <ExternalLink href={documentationLinks.bundleAnnotations} text="metadata" />&nbsp;directory
+              (<b><code>bundle/metadata</code></b>), and&nbsp;
+              <ExternalLink href={documentationLinks.bundleDockerfile} text="bundle.Dockerfile" />&nbsp;
+               have been created in the Operator project. </li>
+            </ul>
           </p>
           <p>
-            To version your CRDs alongside your CSV, pass the <code>--update-crds</code> flag to the above command. This
-            will copy your CRDs into the <code>xx.yy.zz</code> subdirectory, effectively creating an Operator bundle for
-            you to register.
+            This will scaffold a CSV on version <b><code>x.y.z</code></b>, which is set in the project <b><code>Makefile</code></b>
+            &nbsp;variable <b><code>VERSION</code></b>. See more details in&nbsp;
+            <ExternalLink href={documentationLinks.sdkQuickstartBundle} text="OLM Integration Bundle Quickstart" />.
           </p>
         </React.Fragment>
       )
@@ -233,7 +252,6 @@ const Contribute: React.FC<ContributePageProps> = ({ history, ...props }) => {
             <ExternalLink
               href={documentationLinks.manualTestingOnKubernetes}
               text="instruction for manual, local testing"
-
             />
             . This will speed up the submission process significantly.
           </p>
